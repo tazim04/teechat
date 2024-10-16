45e3da611fbc84794ea34457bcf3b08f359035ad
tazim04 <35643104+tazim04@users.noreply.github.com>
2024-10-16 12:19:29 -0400

    Revert "frontend config changes"
    This reverts commit dc1152be7202eb07bc45d5260efe607302b9357b.

--- backend/server.js
+++ backend/server.js
@@ -1036,6 +1036,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Listening on port 3000");
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running and listening on port 3000");
 });
