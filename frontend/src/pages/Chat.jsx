26473da659d69cf5df9ce9f3d7623950e04adfab
tazim04 <35643104+tazim04@users.noreply.github.com>
2024-11-12 16:01:57 -0500

    trying without flex-1

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -318,7 +318,7 @@ function Chat({ currentRoom, setCurrentRoom, messages, setMessages }) {
   }, [messageRefs, messages, setMessages, currentRoom, user._id, socket]);
 
   return (
-    <div className="flex md:flex-row flex-1">
+    <div className="flex md:flex-row">
       {/* <ChatBar room={currentRoom} /> Display the chat bar */}
 
       {currentRoom ? ( // Check if the room (recipient) is selected
